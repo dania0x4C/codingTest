df422b769fc3590c7b3cb0a4b0d7ad018a1c2805
이정민 (Lee JungMin) <100522160+dania0x4C@users.noreply.github.com>
2025-11-19 15:11:35 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -3,25 +3,28 @@ from collections import deque
 
 computerNum = int(input())
 computersV = int(input())
-network = [list(map(int, input().strip().split())) for _ in range(computersV)]
+
+network = [[] for _ in range(computerNum + 1)]
+
+for _ in range(computersV):
+    a, b = map(int, input().split())
+    network[a].append(b)
+    network[b].append(a)
+
 visited = [False] * (computerNum + 1)
 visited[1] = True
 q = deque([1])
-def bfs(network, visited):
+
+def bfs():
     count = 0
     while q:
-        select = q.popleft()
+        current = q.popleft()
 
-        for s, e in network:
-            if select == s and visited[e] == False:
-                visited[e] = True
-                q.append(e)
-                count += 1
-            elif select == e and visited[s] == False:
-                visited[s] = True
-                q.append(s)
-                count += 1
-
+        for next in network[current]:
+            if visited[next] == False:
+                visited[next] = True
+                q.append(next)
+                count+=1
     return count
 
-print(bfs(network, visited))
+print(bfs())
